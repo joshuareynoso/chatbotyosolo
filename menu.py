7cd98318fd213137b8905c0f76bd6c472e15b4ae
joshuareynoso <134154349+joshuareynoso@users.noreply.github.com>
2024-08-06 14:25:55 -0300

    chatbot
    inicio del chatbot

---
+++ menu.py
@@ -0,0 +1,12 @@
+#inicio del programa
+def chatbot(respeuesta,opciones):
+    
+    print("Opcion 4.Quien es dueño")
+opciones=["Opcion 1.Donde esta el hospital","Opcion 2.Como puedo sacar un turno","Opcion 3.Horario del hospital "]
+chatbot()
+while True:    
+    Opciones=int(input("Solo las Opciones:"))
+    if Opciones == 1:
+        
+    
+    chatbot(respuesta=respuesta,opciones=opciones):
