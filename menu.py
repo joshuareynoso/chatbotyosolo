c287b259fe35e06a8bdbc2c44726e79757a7c563
joshuareynoso <134154349+joshuareynoso@users.noreply.github.com>
2024-08-09 15:29:31 -0300

    como se hace esto
    naa mentira

--- menu.py
+++ menu.py
@@ -1,12 +1,14 @@
 #inicio del programa
 def chatbot(respeuesta,opciones):
-    
     print("Opcion 4.Quien es dueño")
-opciones=["Opcion 1.Donde esta el hospital","Opcion 2.Como puedo sacar un turno","Opcion 3.Horario del hospital "]
+    opciones=["Opcion 1.Donde esta el hospital","Opcion 2.Como puedo sacar un turno","Opcion 3.Horario del hospital","Opcion 4.Quien es dueño"]
 chatbot()
+class menu():
+
+    chatbot(respuesta=respuesta,opciones=opciones)
+
+
 while True:    
     Opciones=int(input("Solo las Opciones:"))
     if Opciones == 1:
-        
-    
-    chatbot(respuesta=respuesta,opciones=opciones):
+        print("")
