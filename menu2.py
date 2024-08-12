3343115baf6445304cfc20a56dc83cd14f5d2745
joshuareynoso <jbreynoso@escuelasproa.edu.ar>
2024-08-12 02:40:30 -0300

    el convienso

---
+++ menu2.py
@@ -0,0 +1,66 @@
+#INICIO DEL PROGRAMA 
+def Menu():
+    lista=["menu"," 1 donde esta el hospital","2 como puedo sacar un turno","3 horarios de los doctores","4 cuarta pregunta","5 quinta pregunta","SI TUDUDAS no son esta toque 7"]
+    print(lista)
+
+Menu()    
+
+while True:
+    opciones=int(input("ingrese una opcion del 1 al 5 :"))
+    
+    
+    if opciones== 1:
+            print("Hospital Municipal La Falda, 13 de Diciembre 596, X5172 La Falda, Córdoba")
+                
+            opciondevolver=int(input("si quuiere preguntar algo mas presione 6")) 
+            if opciondevolver==6:
+                continue
+              
+    else:
+        break
+    if opciones== 2:
+            print(" puede ingresar a este link y su licitar un turno")
+                
+            opciondevolver=int(input("si quiere preguntar algo mas presione 6")) 
+            if opciondevolver==6:
+                continue
+    else:
+        break
+    
+    if opciones== 3:
+            print("Hospital Municipal La Falda, 13 de Diciembre 596, X5172 La Falda, Córdoba")
+                
+            opciondevolver=int(input("si quuiere preguntar algo mas presione 6")) 
+            if opciondevolver==6:
+                continue
+              
+    else:
+        break
+    if opciones== 4:
+            print("Hospital Municipal La Falda, 13 de Diciembre 596, X5172 La Falda, Córdoba")
+                
+            opciondevolver=int(input("si quuiere preguntar algo mas presione 6")) 
+            if opciondevolver==6:
+                continue
+              
+    else:
+        break
+    if opciones == 5:
+            print("Hospital Municipal La Falda, 13 de Diciembre 596, X5172 La Falda, Córdoba")
+                
+            opciondevolver=int(input("si quuiere preguntar algo mas presione 6")) 
+            if opciondevolver==6:
+                continue
+              
+    else:
+        break
+    if opciones == 7:
+            print("Hospital Municipal La Falda, 13 de Diciembre 596, X5172 La Falda, Córdoba")
+                
+            opciondevolver=int(input("si quuiere preguntar algo mas presione 6")) 
+            if opciondevolver==6:
+                continue
+              
+    else:
+        break
+    
